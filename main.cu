eea64b951ec5ad5149bac8778c5f6f80045c3382
Duane Irvin <irvin93d@gmail.com>
2017-03-15 18:20:49 -0700

    Read file

--- main.cu
+++ main.cu
@@ -4,10 +4,13 @@
 #include <stdio.h>
 #include <string.h>
 #include "md5.hpp"
+#include <fstream>
+#include <errno.h>
 
 #define PASSWORDS_PER_KERNEL 4
 #define MAX_PASSWORD_LEN 256
 #define DIGEST_SIZE 16
+#define BLOCK_DIM 256
 
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
@@ -19,11 +22,11 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 }
 
 __global__ void crackMD5(unsigned char* hash_in, char* pass_set, uint32_t len, char* pass_out) {
-    // TODO
 	unsigned char hash_in_cache[DIGEST_SIZE];
 	memcpy(hash_in_cache, hash_in, DIGEST_SIZE);
 
 	// TODO CREATE LOOP WHER IF IS
+	
     for(int id = threadIdx.x + blockIdx.x*blockDim.x ; id < len ; id += gridDim.x*blockDim.x) {
 		// Init varibles for password test
         char * pass_test = pass_set + MAX_PASSWORD_LEN * id;
@@ -66,6 +69,12 @@ int main(int argc, char const ** argv) {
 
     std::string hash = "5f4dcc3b5aa765d61d8327deb882cf99"; // 'password'
 
+	std::ifstream file("List-john.txt");
+	if(!file) {
+		std::cerr << "Error: " << strerror(errno) << std::endl;
+		return(-1);
+	}
+
     std::cout << hash << std::endl;
 
 	unsigned char hash_in[17];
@@ -81,13 +90,18 @@ int main(int argc, char const ** argv) {
     gpuErrchk(cudaMalloc((void**) &d_hash_in, 16));
     gpuErrchk(cudaMalloc((void**) &d_passwords, PASSWORDS_PER_KERNEL * MAX_PASSWORD_LEN));
 
-	//TODO load all passwords with padding, passwords size = PASSWORDS_PER_KERNEL*MAX_PASSWORD_LEN
+
     const char * dictionary[] = {"kattj'vel", "passwor", "password", "passwords"};
+	//std::string password; 
+	//while (std::getline(file, str)) {
+	//	std::cout << str << std::endl;	
+	//}
+
+	//TODO load all passwords with padding, passwords size = PASSWORDS_PER_KERNEL*MAX_PASSWORD_LEN
 	char passwords[PASSWORDS_PER_KERNEL * MAX_PASSWORD_LEN] = {0};
 	for(int p = 0 ; p < PASSWORDS_PER_KERNEL ; ++p) {
 		strcpy(passwords+p*MAX_PASSWORD_LEN, dictionary[p]);	
 	}
-
 
 	// device variable initializing
     gpuErrchk(cudaMemcpy(d_hash_in, hash_in, 16, cudaMemcpyHostToDevice));
@@ -95,20 +109,15 @@ int main(int argc, char const ** argv) {
 	gpuErrchk(cudaMemset(d_pass_out, 0, MAX_PASSWORD_LEN));
 
 	// run crack
-	dim3 grid_dim(1,0,0);
-	dim3 block_dim(1,0,0); // TODO fix number of threads, make grid dynamic
-    crackMD5<<<3,3>>>(d_hash_in, d_passwords, PASSWORDS_PER_KERNEL, d_pass_out);
-   
+    crackMD5<<<(PASSWORDS_PER_KERNEL+BLOCK_DIM-1)/BLOCK_DIM,BLOCK_DIM>>>(d_hash_in, d_passwords, PASSWORDS_PER_KERNEL, d_pass_out);
 	cudaError_t err = cudaGetLastError();
 	if(err != cudaSuccess) {
 		printf("ERROR: %s\n", err);
     }
 
-
 	// retrieve result
     unsigned char result[MAX_PASSWORD_LEN] = {0};
     cudaMemcpy(result, d_pass_out, MAX_PASSWORD_LEN, cudaMemcpyDeviceToHost);
-  
 
   	// free device memory
 	gpuErrchk(cudaFree(d_pass_out));
@@ -121,5 +130,6 @@ int main(int argc, char const ** argv) {
 	// print result
     std::cout << hexdigest(hash_in) << std::endl;
     std::cout << "Password is: " << result << std::endl; 
-    return 0;
+   
+	return 0;
 }
